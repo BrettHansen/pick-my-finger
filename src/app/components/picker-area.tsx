f963622c4b7865e25a37f02b4d86e233d9aa0d88
Brett Hansen <brett@brett.local>
2026-01-11 17:39:41 -0800

    Implemented random picker logic

--- src/app/components/picker-area.tsx
+++ src/app/components/picker-area.tsx
@@ -1,17 +1,78 @@
 'use client';
 
 import { Touch, TouchEvent, useState } from 'react';
+import { useTimer } from 'react-timer-hook';
+
+const componentToHex = (component: number) => {
+    const hex = component.toString(16);
+    return hex.length == 1 ? '0' + hex : hex;
+};
+
+const rgbToHex = (r: number, g: number, b: number) => `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
+
+type PickerState = 'idle' | 'countdown' | 'picked';
 
 interface TouchTracker {
     x: number;
     y: number;
+    color: string;
 }
 
 const TOUCH_TARGET_COLORS = ['#E50000', '#FF8D00', '#FFEE00', '#028121', '#004CFF', '#770088'];
 
 export const PickerArea: React.FC = () => {
     const [debugMessages, setDebugMessages] = useState<string[]>([]);
+
+    const [pickerState, setPickerState] = useState<PickerState>('idle');
     const [touchTrackers, setTouchTrackers] = useState<TouchTracker[]>([]);
+
+    const onCountdownEnd = () => {
+        if (pickerState === 'countdown') {
+            const pickedTouchTrackerIndex = Math.floor(Math.random() * touchTrackers.length);
+
+            setTouchTrackers([touchTrackers[pickedTouchTrackerIndex]]);
+            setPickerState('picked');
+        }
+    };
+
+    const { isRunning, seconds, pause, restart } = useTimer({
+        expiryTimestamp: new Date(),
+        autoStart: false,
+        onExpire: onCountdownEnd,
+        interval: 100,
+    });
+
+    const restartCountdown = (touches: Touch[]) => {
+        const time = new Date();
+        time.setSeconds(time.getSeconds() + 5);
+
+        restart(time, true);
+
+        setPickerState('countdown');
+        updateTouchTrackers(touches);
+    };
+
+    const cancelCountdown = (touches: Touch[]) => {
+        pause();
+
+        setPickerState('idle');
+        updateTouchTrackers(touches);
+    };
+
+    const updateTouchTrackers = (touches: Touch[]) => {
+        setTouchTrackers(
+            touches.map(({ clientX, clientY }, index) => ({
+                x: clientX,
+                y: clientY,
+                color: TOUCH_TARGET_COLORS[index],
+            })),
+        );
+    };
+
+    const resetCountdown = () => {
+        setPickerState('idle');
+        setTouchTrackers([]);
+    };
 
     const onTouchEvent = (e: TouchEvent) => {
         const touches: Touch[] = [];
@@ -19,27 +80,65 @@ export const PickerArea: React.FC = () => {
         for (let index = 0; index < e.touches.length; index += 1) {
             touches.push(e.touches.item(index));
         }
+        setDebugMessages(
+            touches.map(
+                ({ clientX, clientY }, index) =>
+                    `${TOUCH_TARGET_COLORS[index]}: ${Math.round(clientX)}, ${Math.round(clientY)}`,
+            ),
+        );
 
-        setTouchTrackers(touches.map(({ clientX, clientY }) => ({ x: clientX, y: clientY })));
-        setDebugMessages(touches.map(({ clientX, clientY }) => `${Math.round(clientX)}, ${Math.round(clientY)}`));
+        switch (pickerState) {
+            case 'idle':
+                if (touches.length > 1) {
+                    restartCountdown(touches);
+                }
+            case 'countdown':
+                if (touches.length !== touchTrackers.length) {
+                    if (touches.length > 1) {
+                        restartCountdown(touches);
+                    } else {
+                        cancelCountdown(touches);
+                    }
+                } else {
+                    updateTouchTrackers(touches);
+                }
+            case 'picked':
+                if (touches.length === 0) {
+                    resetCountdown();
+                }
+        }
     };
+
+    const transparency = touchTrackers.length * 24;
+    const containerBackgroundColor = rgbToHex(transparency, transparency, transparency);
 
     return (
         <div id="container" className="flex h-screen">
             <div
                 id="interaction-area"
-                className="flex-1 touch-none select-none"
+                className="flex-1 touch-none pointer-none select-none"
                 onTouchStart={onTouchEvent}
                 onTouchMove={onTouchEvent}
                 onTouchEnd={onTouchEvent}
                 onTouchCancel={onTouchEvent}
+                style={{ backgroundColor: containerBackgroundColor }}
             >
-                {touchTrackers.map(({ x, y }, index) => (
-                    <TouchTracker key={`touch-tracker-${index}`} x={x} y={y} color={TOUCH_TARGET_COLORS[index]} />
+                {touchTrackers.map(({ x, y, color }, index) => (
+                    <TouchTracker key={`touch-tracker-${index}`} x={x} y={y} color={color} />
                 ))}
             </div>
-            <div id="debug-area" className="w-40 overflow-scroll whitespace-pre border border-white rounded-xl">
-                <code>{debugMessages.join('\n')}</code>
+            <div
+                id="debug-area"
+                className="absolute top-0 w-screen touch-none pointer-none select-none whitespace-pre bg-gray-800 text-white"
+            >
+                <code>
+                    {[
+                        containerBackgroundColor,
+                        pickerState,
+                        `Picking in: ${isRunning ? seconds : '5'}s`,
+                        ...debugMessages,
+                    ].join('\n')}
+                </code>
             </div>
         </div>
     );
